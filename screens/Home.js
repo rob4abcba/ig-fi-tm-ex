18163072a71a55bb89fce324299c119ec777b44d
rob4abcba <rob_abcba@yahoo.com>
2019-08-01 16:58:21 -0700

    Everything else

---
+++ screens/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+// import console = require('console');
+
+export default class Home extends React.Component {
+  state = {
+    count: 10
+  };
+
+  add = () => {
+    console.log("Home.js: add function");
+    let num = this.state.count + 1;
+    this.setState({ count: num });
+  };
+  subtract = () => {
+    console.log("Home.js: subtract function");
+    let num = this.state.count - 1;
+    this.setState({ count: num });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Counter {this.state.count} </Text>
+        <Button title="Add" onPress={() => this.add()} />
+        <Button title="Subtract" onPress={() => this.subtract()} />
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
