47f58d2e5346ff049b2db55a6fe9103cfd67d189
R <r@Rs-MacBook-Air.local>
2019-08-02 16:55:11 -0700

    Add action UPDATE_PASSWORD

--- reducers/index.js
+++ reducers/index.js
@@ -15,6 +15,8 @@ const user = (state = {}, action) => {
     switch (action.type) {
         case "UPDATE_EMAIL":
             return {...state, email: action.payload}
+        case "UPDATE_PASSWORD":
+            return {...state, password: action.payload}
         default:
             return state
     }
