2363c9f06f8e1b390e6aea67d99e851ee5f3c374
R <r@Rs-MacBook-Air.local>
2019-08-02 16:58:35 -0700

    Add redux-logger, redux-thunk, & Middleware.

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import SwitchNavigator from "./navigation/SwitchNavigator";
 import reducer from "./reducers";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-
+import thunkMiddleware from "redux-thunk";
+import logger from "redux-logger";
+const middleware = applyMiddleware(thunkMiddleware, logger)
 const store = createStore(reducer);
 
 export default class App extends React.Component {
