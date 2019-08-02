2ba2cc8ff67cf5a662894d7277b7778eeca6605e
R <r@Rs-MacBook-Air.local>
2019-08-01 20:49:14 -0700

    Add Profile.js and Upload.js to TabNavigator.js

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
 import Home from "../screens/Home.js"
 import Search from "../screens/Search.js"
+import Upload from "../screens/Upload.js"
 import Activity from "../screens/Activity.js"
-
+import Profile from "../screens/Profile.js"
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 const TabNavigator = createBottomTabNavigator({
   Home: Home,
   Search: Search,
+  Upload: Upload,
+  Profile: Profile,
   Activity: Activity
 });
 
