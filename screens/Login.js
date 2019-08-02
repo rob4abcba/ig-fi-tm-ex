b6194214c28b3824b4c7932a8f07f6ce8e09f557
R <r@Rs-MacBook-Air.local>
2019-08-02 16:51:04 -0700

    Implement updatePassword TextInput

--- screens/Login.js
+++ screens/Login.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Text, View, TextInput } from "react-native";
-import styles from "../styles.js"
+import styles from "../styles.js";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { updateEmail } from "../actions/user.js"
+import { updateEmail, updatePassword } from "../actions/user.js";
 
 class Login extends React.Component {
   render() {
@@ -11,27 +11,31 @@ class Login extends React.Component {
       <View style={styles.container}>
         <Text> Login </Text>
         <TextInput
-        value={this.props.user}
-        onChangeText = { input => this.props.updateEmail(input)}
-        placeholder = "email"
+          value={this.props.newuser}
+          onChangeText={input => this.props.updateEmail(input)}
+          placeholder="email"
         />
         <TextInput
-        value="value"
-        onChangeText = { input => console.log(input)}
-        placeholder = "password"
+          value={this.props.newpassword}
+          onChangeText={input => this.props.updatePassword(input)}
+          placeholder="password"
         />
       </View>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators( {updateEmail}, dispatch )
-  }
-  const mapStateToProps = (state) => {
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ updateEmail, updatePassword }, dispatch);
+};
+const mapStateToProps = state => {
   return {
-    user: state
-  }
-  }
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(Login)
+    newuser: state.user,
+    newpassword: state.password
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
