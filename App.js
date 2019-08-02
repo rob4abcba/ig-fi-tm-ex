7b818340f0da6cda1b92003fb9aea4a0af5f0c3e
R <r@Rs-MacBook-Air.local>
2019-08-02 05:43:39 -0700

    import redux react-redux. Create store fr reducers

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import TabNavigator from "./navigation/TabNavigator";
+import reducer from "./reducers";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(reducer);
 
 export default class App extends React.Component {
   render() {
     return (
+      <Provider store={store}>
         <TabNavigator />
+        </Provider>
     );
   }
 }
